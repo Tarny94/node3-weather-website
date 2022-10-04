53e27faf469ee1637bf9c8061d8b5a3ac0f80e09
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-10-04 17:31:49 +0300

    Solve port crash

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ console.log("1", __dirname);
 console.log("2", __filename);
 
 const app = express();
-const port = process.env.POST || 3000;
+const port = process.env.PORT || 3000;
 
 //Define path for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
